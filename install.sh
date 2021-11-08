41c52111d50546bb6cd60bf3861a32676a38a5c2
RTM <93855730+testnet3@users.noreply.github.com>
2021-11-08 08:57:16 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+COIN_NAME='raptoreum'
+BOOTSTRAP_TAR='https://github.com/testnet3/RTMnode/releases/download/latest/bootstrap.tgz'
+WALLET_TAR='https://github.com/Raptor3um/raptoreum/releases/download/1.3.15.99/raptoreum_1.3.15.99_ubuntu18_64.tar.gz'
+CONFIG_DIR='.raptoreumcore'
+CONFIG_FILE='raptoreum.conf'
+PORT='10228'
+COIN_DAEMON='raptoreumd'
+COIN_CLI='raptoreum-cli'
+COIN_CLI_QT='raptoreum-qt'
+COIN_TX='raptoreum-tx'
+COIN_PATH='/usr/local/bin'
+USERNAME='$(whoami)'
+START_QT_ANS=""
+
+echo -e "             Based on Raptoreum node install script by dk808 from AltTank"
+echo -e "             Smartnode healthcheck by Delgon"
+echo -e ""
+echo -e "             tRTM Node setup starting, press [CTRL-C] to cancel."
+sleep 5
+if [ "$USERNAME" = "root" ]; then
+  echo -e "You are currently logged in as root, please switch to a sudo user."
+  exit
+fi
+
+function wipe_clean() {
+  echo -e "Removing any instances of tRTM..."
+  sudo $COIN_CLI_QT stop 
+  sudo killall $COIN_DAEMON 
+  sudo rm /usr/local/bin/$COIN_NAME* 
+  rm -rf $HOME/$CONFIG_DIR 
+}
+
+function ip_confirm() {
+  echo -e "Detecting IP address being used..." && sleep 1
+  WANIP=$(curl https://ifconfig.co/ip)
+  if ! whiptail --yesno "Detected IP address is $WANIP is this correct?" 8 60; then
+      WANIP=$(whiptail --inputbox "        Enter IP address" 8 36 3>&1 1>&2 2>&3)
+  fi
+  }
+
+function install_packages() { 
+  echo -e "Installing Packages..."
+  sudo apt-get install pwgen tar -y
+  echo -e "Packages complete..."
+}
+
+smartnodeblsprivkey=""
+function create_conf() {
+  if [[ ! -z $1 ]]; then
+    while [[ -z $smartnodeblsprivkey ]]; do
+      smartnodeblsprivkey=$(whiptail --inputbox "Enter your SmartNode operatorSecret key" 8 75 3>&1 1>&2 2>&3)
+    done
+    return
+  fi
+  while [[ -z $smartnodeblsprivkey ]]; do
+    smartnodeblsprivkey=$(whiptail --inputbox "Enter your SmartNode operatorSecret key" 8 75 3>&1 1>&2 2>&3)
+  done
+  if [ -f $HOME/$CONFIG_DIR/$CONFIG_FILE ]; then
+    echo -e "Existing conf file found backing up to $COIN_NAME.old ..."
+    mv $HOME/$CONFIG_DIR/$CONFIG_FILE $HOME/$CONFIG_DIR/$COIN_NAME.old;
+  fi
+  RPCUSER=$(pwgen -1 8 -n)
+  PASSWORD=$(pwgen -1 20 -n)
+  echo -e "Creating Conf File..."
+  sleep 1
+  mkdir $HOME/$CONFIG_DIR > /dev/null 2>&1
+  touch $HOME/$CONFIG_DIR/$CONFIG_FILE
+  cat << EOF > $HOME/$CONFIG_DIR/$CONFIG_FILE
+rpcuser=$RPCUSER
+rpcpassword=$PASSWORD
+rpcallowip=127.0.0.1
+rpcbind=127.0.0.1
+port=$PORT
+server=1
+daemon=1
+listen=1
+smartnodeblsprivkey=$smartnodeblsprivkey
+externalip=$WANIP
+maxconnections=128
+EOF
+}
+
+function install_bins() {
+  echo -e "Installing latest binaries..."
+  mkdir rtemp
+  curl -L $WALLET_TAR | tar xz -C ./rtemp
+  sudo mv ./rtemp/$COIN_DAEMON ./rtemp/$COIN_CLI ./rtemp/$COIN_CLI_QT ./rtemp/$COIN_TX $COIN_PATH
+  sudo chmod 755 ${COIN_PATH}/${COIN_NAME}*
+  rm -rf rtemp
+}
+
+function bootstrap() {
+    echo -e "Downloading wallet bootstrap please be patient..."
+  mkdir ~/$CONFIG_DIR
+    curl -L $BOOTSTRAP_TAR | tar xz -C $HOME/$CONFIG_DIR
+ }  
+
+function start_qt() {
+  if [[ ! -z $1 ]]; then
+    if whiptail --yesno "Would you like to start QT wallet?" 8 42; then
+      START_QT_ANS=1
+    fi
+  elif [[ ! -z $START_QT_ANS ]]; then
+clear
+ echo -e ""
+ echo -e ""
+ echo -e ""
+ echo -e "protx quick_setup cтрока из smatrnode outputs 1  IP port кошелек из listaddressbalances"
+ echo -e ""
+ echo -e "smartnode outputs "
+ echo -e ""
+ echo -e "listaddressbalances "
+ echo -e ""
+ echo -e "operatorSecret 211fc0a9ee0496c6045d0f"
+ echo -e ""
+read -p "Press Enter key to start qt wallet ... "
+
+echo -e "Starting qt-wallet please be patient..."
+    /usr/local/bin/./raptoreum-qt -testnet
+    else
+    echo -e "Skipping wallet starting..."
+  fi
+}
+
+  wipe_clean
+  install_packages
+  install_bins
+  bootstrap
+  ip_confirm
+  start_qt true
+  start_qt
+  create_conf
+ /usr/local/bin/./raptoreumd -testnet
